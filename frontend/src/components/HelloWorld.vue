b5e542d3aac0a007f568357dc5c79c365112e0d2
mumumu <takumu.sato@gmail.com>
2024-06-13 20:57:22 +0900

    リセットボタンを追加しました

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -11,6 +11,7 @@ const count = ref(0);
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count=0">reset</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
